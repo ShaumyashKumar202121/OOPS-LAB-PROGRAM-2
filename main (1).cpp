c5e51dc67c1fcd4600b1dfea8e41ca67fe4f5106
ShaumyashKumar202121 <91329731+ShaumyashKumar202121@users.noreply.github.com>
2022-03-19 08:07:32 +0530

    Add files via upload
    OBJECT ORIENTED PROGRAMMING USING C++ LAB PROGRAM 2 - Program to make the use of inline function.

---
+++ main (1).cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+inline int cube(int x)
+{
+    return x*x*x;
+}
+
+int main()
+
+{
+    cout<<"Our cube is : "<<cube(5)<<endl;
+     
+     cout<<"Our cube is : "<<cube(6)<<endl;;
+    
+      cout<<"Our cube is : "<<cube(7)<<endl;
+      
+       cout<<"Our cube is : "<<cube(8)<<endl;
+
+    return 0;
+}
+
+
